3fefd11542370cc7879157d020dc305c1d923bf6
prani14 <pranaykumarshivas1@gmail.com>
2024-04-19 13:00:15 +0530

    sorting technics

---
+++ sorting/bubble1.java
@@ -0,0 +1,36 @@
+package sorting;
+
+public class bubble1 {
+   
+
+ 
+    public static void sort(int arr[]) {
+        for(int turn=0;turn<arr.length-1;turn++){
+            int swap=0;
+            for(int j=0;j<arr.length-1-turn;j++){
+                //swap
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                 
+                }swap++;
+
+            }System.out.println("number of swaps"+swap);
+        }
+        
+    }public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+"");
+        }System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={2,1,4,5,6,7};
+        sort(arr);
+        print(arr);
+    }
+    
+}
+
+    
+
